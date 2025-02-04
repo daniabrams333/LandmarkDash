2b87d9198e37bf13cbf71a0c3af66b82d99ff75c
Danielle Hampton-Abrams <daniabrams333@gmail.com>
2025-02-04 14:28:12 -0500

    Adding a Content View File so we can set up the SwiftUI files - DHA

---
+++ LandmarkDash/LandmarkDashApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDashApp.swift
+//  LandmarkDash
+//
+//  Created by Danielle Abrams on 2/3/25.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct LandmarkDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+           
+        }
+    }
+}
