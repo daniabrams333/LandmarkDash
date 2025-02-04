2ef4597ac9b2eac2312fa14af9d79f3b53632e03
Lessley <Lessley.Romero.Rodriguez.2006@gmail.com>
2025-02-04 16:40:31 -0500

    adding assets colors

---
+++ LandmarkDash/WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeView.swift
+//  LandmarkDash
+//
+//  Created by Lessly C. Romero-Rodriguez on 2/4/25.
+//
+
+import SwiftUI
+
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            Color.lightyellow.ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
