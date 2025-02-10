ad50ea84af1aa3f9fb86655e83823575f1be59f3
Lessley <Lessley.Romero.Rodriguez.2006@gmail.com>
2025-02-10 15:19:15 -0500

    working fixing error

---
+++ LandmarkDash/welcomepage1.swift
@@ -0,0 +1,18 @@
+//
+//  welcomepage1.swift
+//  LandmarkDash
+//
+//  Created by Lessly C. Romero-Rodriguez on 2/6/25.
+//
+
+import SwiftUI
+
+struct welcomepage1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    welcomepage1()
+}
